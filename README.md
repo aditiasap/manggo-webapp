74bc88a421d4fff1e3cb8c0e91640b30c5ab2d21
Aditia Saputra <aditiasap@gmail.com>
2026-06-05 11:20:21 +0700

    bootstrap the work

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Manga on the Go..
 
+The App to generate Manga character image based on AI text prompt.
+
+Enjoy 👍
+
---
+++ app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return (
+        <p className="fixed top-1/2 left-1/2 text-lg font-semibold text-sky-700">
+            Loading...
+        </p>
+    );
+};
